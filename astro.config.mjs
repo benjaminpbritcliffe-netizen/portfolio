30afac5a91458c94aafe0e1702454e1b50d37c17
benjaminpbritcliffe-netizen <benjamin.p.britcliffe@gmail.com>
2026-04-21 14:34:40 +0100

    feat(initial commit.): :zap:

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.https://benjaminpbritcliffe-netizen.github.io/portfolio/',
   vite: {
     plugins: [tailwindcss()],
   },
